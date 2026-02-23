1d4cb9b2fd35573254cb7f94c38d1c3050aa1f90
li leo <faboto@yahoo.com>
2026-02-23 12:09:59 +0100

    fix

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from main import app
 from fastapi.testclient import TestClient
-from src.main import app
 
+print(app)
 client = TestClient(app)
 
 def test_root():
