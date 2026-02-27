68b2af154a90693b09632990794235f04caa5e1b
li leo <faboto@yahoo.com>
2026-02-27 10:36:08 +0100

    fix integration

--- etl/scripts/extract_script/test_extract_integration.py
+++ etl/scripts/extract_script/test_extract_integration.py
@@ -1,13 +1,13 @@
 import tempfile
-from scripts.extract_script.gtfs_download import download_gtfs_file
-from scripts.extract_script.gtfs_utils import extract_gtfs_zip
+from scripts.extract_script.gtfs_download import download_file
+from scripts.extract_script.gtfs_download import extract_zip
 from scripts.extract_script.gtfs_api import get_latest_gtfs_from_api
 import os
 def test_download_gtfs_file_real():
     url = "https://transport-data-gouv-fr-resource-history-prod.cellar-c2.services.clever-cloud.com/83582/83582.20260227.001749.202024.zip"
     with tempfile.TemporaryDirectory() as tmpdir:
         dest = f"{tmpdir}/feed.zip"
-        download_gtfs_file(url, dest)
+        download_file(url, dest)
         assert os.path.exists(dest)
         assert os.path.getsize(dest) > 0
 
@@ -24,7 +24,7 @@ def test_extract_gtfs_zip_real():
     url = "https://transport-data-gouv-fr-resource-history-prod.cellar-c2.services.clever-cloud.com/83582/83582.20260227.001749.202024.zip"
     with tempfile.TemporaryDirectory() as tmpdir:
         zip_path = f"{tmpdir}/feed.zip"
-        download_gtfs_file(url, zip_path)
+        download_file(url, zip_path)
         extract_path = f"{tmpdir}/extracted"
-        extract_gtfs_zip(zip_path, extract_path)
+        extract_zip(zip_path, extract_path)
         assert os.path.exists(f"{extract_path}/agency.txt")
