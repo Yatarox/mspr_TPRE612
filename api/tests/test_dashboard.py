7dd15f720781d915f7f2dc956d6e3cfd113b90d2
li leo <faboto@yahoo.com>
2026-03-18 14:19:46 +0100

    fix

--- api/tests/test_dashboard.py
+++ api/tests/test_dashboard.py
@@ -3,12 +3,12 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from fastapi.testclient import TestClient
-from unittest.mock import patch, AsyncMock
+from unittest.mock import patch
 from main import app
 
 client = TestClient(app)
 
-@patch("services.dashboard_service.get_stats_by_service_type")
+@patch("api.routes.dashboard.dashboard_service.get_stats_by_service_type")
 def test_get_stats_by_service_type_endpoint(mock_service):
     mock_service.return_value = [
         {"service_type": "Jour", "trip_count": 150}
